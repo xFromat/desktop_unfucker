f6010a693d96b9d25858702ede68aa38546c215b
xFromat <dev@piotrferst.com>
2025-10-20 23:16:56 +0200

    Gave up with precission, got epsilon value

--- tools/Get-DesktopIconGridFit.ps1
+++ tools/Get-DesktopIconGridFit.ps1
@@ -1,18 +1,18 @@
 function Get-DesktopIconGridFit {
-		<#
-		.SYNOPSIS
-			Calculates how many desktop icons can fit on the virtual desktop (all screens).
+	<#
+	.SYNOPSIS
+		Calculates how many desktop icons can fit on the virtual desktop (all screens).
 
-		.OUTPUTS
-			[PSCustomObject] with:
-				- TotalWidth: Total virtual desktop width (pixels)
-				- MaxHeight: Max height of any connected monitor (pixels)
-				- IconWidth: Icon grid width (pixels)
-				- IconHeight: Icon grid height (pixels)
-				- IconsPerRow: Number of icons that fit per row
-				- IconsPerColumn: Number of icons that fit per column
-				- TotalIcons: Total icons that can fit
-		#>
+	.OUTPUTS
+		[PSCustomObject] with:
+			- TotalWidth: Total virtual desktop width (pixels)
+			- MaxHeight: Max height of any connected monitor (pixels)
+			- IconWidth: Icon grid width (pixels)
+			- IconHeight: Icon grid height (pixels)
+			- IconsPerRow: Number of icons that fit per row
+			- IconsPerColumn: Number of icons that fit per column
+			- TotalIcons: Total icons that can fit
+	#>
 
 	Add-Type -AssemblyName System.Windows.Forms
 	$output = @()
@@ -23,10 +23,10 @@ function Get-DesktopIconGridFit {
 		$usableHeight = $screen.WorkingArea.Height
 
 		# Get icon spacing from registry
-		$metrics = Get-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name IconSpacing, IconVerticalSpacing
+		$metrics = Get-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name IconSpacing, IconVerticalSpacing, BorderWidth
 		$iconSize = (Get-ItemProperty -path HKCU:\Software\Microsoft\Windows\Shell\Bags\1\Desktop -name IconSize).IconSize
-		$iconWidth = [math]::Max(([math]::Abs($metrics.IconSpacing) / 15), $iconSize)
-		$iconHeight = [math]::Max(([math]::Abs($metrics.IconVerticalSpacing) / 15), $iconSize)
+		$iconWidth = [math]::Max(([math]::Abs($metrics.IconSpacing) / [math]::Abs($metrics.BorderWidth)), $iconSize)
+		$iconHeight = [math]::Max(([math]::Abs($metrics.IconVerticalSpacing) / [math]::Abs($metrics.BorderWidth)), $iconSize)
 
 		# Compute fit
     	$columns = [math]::Floor($usableWidth / $iconWidth)
@@ -35,13 +35,13 @@ function Get-DesktopIconGridFit {
 
     	# Return result as object
 		$output += [PSCustomObject]@{
-        	Width			= $usableWidth
-        	Height			= $usableHeight
-        	IconWidth       = [math]::Floor($iconWidth)
-        	IconHeight      = [math]::Floor($iconHeight)
-        	Columns 		= $columns
-			Rows  			= $rows
-        	TotalIcons      = $totalIcons
+			Width = $usableWidth
+        	Height = $usableHeight
+        	IconWidth = [math]::Floor($iconWidth)
+        	IconHeight = [math]::Floor($iconHeight)
+        	Columns = $columns
+			Rows = $rows
+        	TotalIcons = $totalIcons
     	}
 	}
 	return $output

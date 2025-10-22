af3ae074269d2764e77ceda085625c69a81b0455
xFromat <dev@piotrferst.com>
2025-10-23 00:22:43 +0200

    Fixed directory creating in simulation

--- tests/populate-screen.ps1
+++ tests/populate-screen.ps1
@@ -1,22 +1,36 @@
 param (
-    [int]$iconsTotal = 0,
+	[int]$iconsTotal = 0,
     [string]$outputListPath = ".\generated_files.txt",
-    [string]$desktopPath = [Environment]::GetFolderPath('Desktop')
+    [string]$desktopPath = [Environment]::GetFolderPath('Desktop'),
+	[int]$filesPercent = 95
 )
 
-$exts =  @(".png", ".txt", ".lnk", ".pdf")
+# Define a list of real file extensions
+$exts = @(
+	".txt", ".csv", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx",
+    ".pdf", ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg",
+    ".mp3", ".wav", ".flac", ".mp4", ".avi", ".mkv", ".mov",
+    ".zip", ".rar", ".7z", ".tar", ".gz",
+    ".exe", ".msi", ".bat", ".ps1", ".sh", ".py", ".js", ".html", ".css", ".json", ".xml"
+)
+
 $generatedFiles = @()
 
-for ($i = 1; $i -le (($iconsTotal - 2)*3); $i++) {
-    $randomExt = Get-Random -InputObject $exts  
-    $filePath = Join-Path -Path "$desktopPath\" -ChildPath ("{0}{1}{2}" -f "test", $i, $randomExt)
-
+for ($i = 1; $i -le (($iconsTotal -15) * 8); $i++) {
+	$filePath = Join-Path -Path "$desktopPath\" -ChildPath ("{0}{1}" -f "test", $i)
+	$j = (Get-Random -Minimum 0 -Maximum 100)
+	if ($j -lt $filesPercent) {
+		$randomExt = Get-Random -InputObject $exts
+		$filePath += $randomExt
+    	New-item -Path $filePath -ItemType File -Force
+	}
+	else {
+		New-Item -Path $filePath -ItemType Directory -Force 
+	}
     "$filePath"
-
-    New-item -Path $filePath -ItemType File -Force
-
     $generatedFiles += $filePath
 }
 
 # Save list to output file
 $generatedFiles | Set-Content -Path $outputListPath
+
